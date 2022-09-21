27505cce5c4e3c0ba94049b3a98c395d6f2c8cee
AleksKostadinov <kostadinov12@gmail.com>
2022-09-22 02:08:42 +0300

    Created Portfolio model

--- main/models.py
+++ main/models.py
@@ -83,3 +83,29 @@ class Media(models.Model):
     def __str__(self):
         return self.name
 
+class Portfolio(models.Model):
+    
+    class Meta:
+        verbose_name_plural = ("Portfolio Profiles")
+        verbose_name = ("Portfolio")
+        ordering =['name']
+    date = models.DateTimeField(blank=True, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    body = RichTextField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True, upload_to='portfolio')
+    slug = models.SlugField(blank=True, null=True)
+    is_active = models.BooleanField(blank=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slig = slugify(self.name)
+            super(Portfolio, self).save(*args, **kwargs)
+            
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/portfolio/{self.slug}'
+    
+    
